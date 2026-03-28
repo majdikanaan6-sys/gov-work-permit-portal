56330b74b2e0402cdf741fc53a1657e1bae6514d
Majdi Kanaan <majdikanaan6@gmail.com>
2026-03-28 14:53:15 -0700

    test server back

--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,11 @@ if (!PORT) {
   throw new Error("PORT is not defined");
 }
 
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
