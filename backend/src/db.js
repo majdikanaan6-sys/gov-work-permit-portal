e1c525b3f60b70db28b02ee028e561922ed1409c
Majdi Kanaan <majdikanaan6@gmail.com>
2026-03-28 12:59:08 -0700

    DB fixes

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "work_permit_portal", // from pgAdmin
-  password: "Ammsterdam7", // your password
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
@@ -13,3 +12,4 @@ module.exports = pool;
 pool.connect()
   .then(() => console.log("✅ Database connected"))
   .catch(err => console.error("❌ DB error:", err));
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
