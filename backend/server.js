5644c18f3ca5222002e8bbe0c61fcc58a8e5d6cd
Majdi Kanaan <majdikanaan6@gmail.com>
2026-03-29 09:16:42 -0700

    test ping

--- backend/server.js
+++ backend/server.js
@@ -75,6 +75,10 @@ app.get("/test-db", async (req, res) => {
   }
 });
 
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
