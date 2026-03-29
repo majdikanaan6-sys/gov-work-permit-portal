85a8887ab58669f25f2047fe9ccba14bdb552e46
Majdi Kanaan <majdikanaan6@gmail.com>
2026-03-29 12:18:20 -0700

    app onlisten

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,6 @@ app.use((err, req, res, next) => {
 // ✅ Correct PORT handling
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
