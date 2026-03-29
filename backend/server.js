e6781e1323651716a10c83249259d878384f5215
Majdi Kanaan <majdikanaan6@gmail.com>
2026-03-29 12:54:44 -0700

    remove 511

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use((err, req, res, next) => {
 // ✅ Correct PORT handling
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", 511, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
