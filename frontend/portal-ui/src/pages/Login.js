12b3816f93852c79dab14e8daea0b9786d67caff
Majdi Kanaan <majdikanaan6@gmail.com>
2026-03-30 11:34:41 -0700

    Login employer

--- frontend/portal-ui/src/pages/Login.js
+++ frontend/portal-ui/src/pages/Login.js
@@ -13,8 +13,8 @@ const submit=async()=>{
 
 try{
 
-const res = await axios.post(
-"http://localhost:5000/api/auth/login",
+await axios.post(
+"https://gov-work-permit-portal-production.up.railway.app/api/auth/register",
 form
 );
 
