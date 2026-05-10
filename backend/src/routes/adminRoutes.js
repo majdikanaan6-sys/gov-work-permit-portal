69c8d7bc454fe379c4223903ceff41495b6eefd4
Majdi Kanaan <majdikanaan6@gmail.com>
2026-05-10 03:24:53 -0700

    cloudin newpub

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
+
 
 const router = express.Router();
 
@@ -10,17 +10,13 @@ const cloudinary = require("../config/cloudinary");
 const { CloudinaryStorage } =
   require("multer-storage-cloudinary");
 
-  
+
 const {
   uploadInvoice,
 } = require("../controllers/adminController");
 
 
 
-// Create folder if missing
-if (!fs.existsSync(uploadPath)) {
-  ;
-}
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -39,41 +35,5 @@ router.post(
   uploadInvoice
 );
 
-router.get("/download-invoice/:filename", (req, res) => {
 
-  try {
-
-    const fs = require("fs");
-
-    const filePath = path.resolve(
-      "uploads",
-      "invoices",
-      req.params.filename
-    );
-
-    console.log("DOWNLOAD PATH:", filePath);
-
-    const exists = fs.existsSync(filePath);
-
-    console.log("FILE EXISTS?", exists);
-
-    if (!exists) {
-      return res.status(404).json({
-        error: "File not found"
-      });
-    }
-
-    return res.download(filePath);
-
-  } catch (err) {
-
-    console.error("DOWNLOAD ERROR:", err);
-
-    return res.status(500).json({
-      error: "Internal Server Error"
-    });
-
-  }
-
-});
 module.exports = router;
